74cd54fdd0a809fb776caf3227d3114640dc61ad
Daniel-TheProgrammer <njid18753@gmail.com>
2022-05-29 18:01:21 +0100

    cart screen

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,8 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const Cart(),
+      home:   Cart(),
     );
   }
 }
+
