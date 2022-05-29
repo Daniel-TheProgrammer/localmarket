513751695bb18fd34604fe7c01d3a73172e0bbd8
Daniel-TheProgrammer <njid18753@gmail.com>
2022-05-29 17:11:22 +0100

    returned cart screen in main

--- lib/screens/user_info.dart
+++ lib/screens/user_info.dart
@@ -5,6 +5,10 @@ class UserInfo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Center(
+        child: Text('User Info')
+      ),
+    );
   }
 }
